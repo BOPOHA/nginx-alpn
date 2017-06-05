99d03da1e766f832bf8a7fccecbafc39b8c25bd3
Anatolii Vorona <vorona.tolik@gmail.com>
2017-06-05 23:48:32 +0200

    new mock strategy, part two

--- contrib/nginx.spec
+++ contrib/nginx.spec
@@ -98,7 +98,7 @@ a mail proxy server.
 %prep
 tar -zxf %{_sourcedir}/nginx-%{main_version}.tar.gz -C %{_sourcedir}
 find %{_sourcedir}
-tar -zxf %{_sourcedir}/nginx-%{main_version}/openssl-1.1.0e.tar.gz -C %{_builddir}
+tar -zxf %{_sourcedir}/%{name}-%{main_version}/openssl-1.1.0e.tar.gz -C %{_builddir}
 
 %setup
 tar --strip-components=1 -zxf %{_sourcedir}/%{name}-%{version}/nginx-%{main_version}.tar.gz
--- contrib/update.bash
+++ contrib/update.bash
@@ -42,7 +42,7 @@ sed -i "s#^\%if 0\%{?rhel} == 7#\%if ( 0\%{\?rhel} == 7 ) || ( 0\%{?fedora} >= 1
 sed -i "s|^\%prep|\%prep\n\
 tar -zxf \%{_sourcedir}/nginx-\%{main_version}.tar.gz -C \%{_sourcedir}\n\
 find \%{_sourcedir}\n\
-tar -zxf \%{_sourcedir}/nginx-\%{main_version}/$OPENSSL.tar.gz -C \%{_builddir}\n\
+tar -zxf \%{_sourcedir}/\%{name}-\%{main_version}/$OPENSSL.tar.gz -C \%{_builddir}\n\
 |" $PRJDIR/contrib/*.spec
 
 sed -i "s|^\%setup -q$|\%setup\n\
